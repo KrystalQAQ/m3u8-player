57a790199ef063ab2081528e61286a90536ad936
tanyu <2542532345@qq.com>
2025-09-21 15:55:03 +0800

    log

--- api/log.js
+++ api/log.js
@@ -15,7 +15,7 @@ const json = (req) => {
   });
 };
 
-module.exports = async (req, res) => {
+export default async (req, res) => {
   if (req.method !== 'POST') {
     return res.status(405).send('Method Not Allowed');
   }
