4a49c610dda0e30fad8ff103e70cb78b6a856386
tanyu <red-krystal@outlook.com>
2025-09-22 10:49:59 +0800

    seo

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { sitemap } from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    sitemap({
+      hostname: 'https://m3u8-player.krystal.fit',
+      dynamicRoutes: ['/', '/krystal'],
+    }),
+  ],
   server: {
     proxy: {
       '/api': {
